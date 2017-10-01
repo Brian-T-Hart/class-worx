ddfd2278e5a7d66fc0896756abe8ba1d7b7b9dca
Brian Hart <brianhart@dhcp-v088-235.mobile.uci.edu>
2017-10-01 16:57:54 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,7 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var methodOverride = require("method-override");
+
+var PORT = process.env.PORT || 3000;
+
+var app = express();

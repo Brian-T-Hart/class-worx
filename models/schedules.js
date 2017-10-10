fb90fdc91cf51bacca7243777c24398165469557
mjbelanic <mjbelanic@aol.com>
2017-10-09 21:16:32 -0700

    Some minor change to schedule

--- models/schedules.js
+++ models/schedules.js
@@ -6,25 +6,11 @@ module.exports = function(sequelize, DataTypes) {
             autoIncrement: true,
             allowNull: false,
           },
-<<<<<<< HEAD
-          schedule_period: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            validate: {
-              len: [1,2]
-            }
-          },
-=======
->>>>>>> master
     },
   { timestamps: false });
     schedules.associate = function(models) {
           schedules.belongsTo(models.students, {
-<<<<<<< HEAD
-              onDelete: "CASCADE",
-=======
                 onDelete: "CASCADE"
->>>>>>> master
           })
           schedules.belongsTo(models.classes, {
             onDelete: "CASCADE"

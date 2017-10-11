f93d917f6eac17e533e8abcc49e4a8827c437d3d
Brian Hart <brianhart@dhcp-v088-235.mobile.uci.edu>
2017-10-11 12:09:14 -0700

    fixed fixes

--- models/index.js
+++ models/index.js
@@ -4,7 +4,7 @@ var fs        = require('fs');
 var path      = require('path');
 var Sequelize = require('sequelize');
 var basename  = path.basename(__filename);
-var env       = process.env.JAWSDB_URL || 'development';
+var env       = process.env.NODE_ENV || 'development';
 var config    = require(__dirname + '/../config/config.json')[env];
 var db        = {};
 

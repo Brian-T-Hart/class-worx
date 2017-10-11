48eba593134c2b924b9c15914955ab5b1eea3485
mjbelanic <mjbelanic@aol.com>
2017-10-11 12:55:51 -0700

    handlebar alterations

--- controller/api.routes.js
+++ controller/api.routes.js
@@ -1,28 +1,32 @@
 var express = require("express");
-var passport = require("passport");
-var db = require("../models");
 var path = require("path");
 var router = express.Router();
+var db = require("../models");
+var passport = require("passport");
 
 // PUT route for updating student points via buttons
-router.put("/:id", function(req, res) {
-    if (req.body.name == +1) {
-        value = parseInt(param)
-    }
-    if(req.body.name == +10){
-        value = parseInt(param)
-    }
-    if(req.body.name == decrement){
-        value= -1
-    }
-    db.students.update({   
-        student_score: student_score + value},
-        {where: 
-            {student_id: req.params.id}
-      }).then(function(result) {
-        res.json(result);
-        // console.log(result);
-      });
-    });
+router.put("/:id/:value", (req, res) => {
+        console.log("Hello");
+        res.redirect("/class/" +req.params.id);
+})
+    
+    // if (req.body.name == +1) {
+    //     value = parseInt(param)
+    // }
+    // if(req.body.name == +10){
+    //     value = parseInt(param)
+    // }
+    // if(req.body.name == decrement){
+    //     value= -1
+    // }
+    // db.students.update({   
+    //     student_score: student_score + value},
+    //     {where: 
+    //         {student_id: req.params.id}
+    //   }).then(function(result) {
+    //     res.json(result);
+    //     // console.log(result);
+    //   });
+    // });
 
 module.exports = router;

b5bc3cd4567b9c61fbbbfb4aca796d5193f2b0b2
mjbelanic <mjbelanic@aol.com>
2017-10-10 18:05:05 -0700

    students for specific class functionality, new student form

--- controller/index.routes.js
+++ controller/index.routes.js
@@ -21,7 +21,8 @@ router.get('/dashboard', (req, res, next) =>{
             }],
             where:{
                 teacherTeacherId: req.user.teacher_id,
-            }
+            },
+            order: db.sequelize.col('class_period')
         }).then(function(results){
             var classList = {classes: results}
             res.render('teacher', classList);
@@ -36,6 +37,7 @@ router.post('/dashboard', (req, res, next) =>{
         db.classes.create({
             class_name: req.body.className,
             class_subject: req.body.classSubject,
+            class_period: req.body.classPeriod,
             class_score: 0,
             class_active: true,
             teacherTeacherId: req.user.teacher_id
@@ -49,19 +51,12 @@ router.post('/dashboard', (req, res, next) =>{
 
 router.get('/class/:id', (req, res, next) =>{
     if(req.isAuthenticated()){
-        //go to dashboard automatically
-        //if statement if teacher has class
-    }else{
-        res.redirect("/account/login");
-    }
-});
-
-
-router.get('/students', (req,res,next) => {
-    if(req.isAuthenticated()){
         db.students.findAll({
             include:[{
                 model: db.schedules,
+                where:{
+                    classClassId: req.params.id
+                },
                 include:[{
                     model: db.classes,
                     include:[{
@@ -82,14 +77,51 @@ router.get('/students', (req,res,next) => {
 });
 
 
-//class and students in class
-// class/:id/students
-// all students of teacher
-// /students
-/*
-specific studnets = /students/:Studentid
-// search based on class id = /students?classid=1
-specifc student from specific class should be the same
- */
+router.get('/students', (req,res,next) => {
+    if(req.isAuthenticated()){
+        db.students.findAll({
+            include:[{
+                model: db.schedules,
+                where:{
+                    classClassId: req.params.id
+                },
+                include:[{
+                    model: db.classes,
+                    include:[{
+                        model: db.teachers,
+                        where:{
+                            teacher_id: req.user.teacher_id,
+                        }
+                    }],
+                }],
+            }],
+        }).then(function(results){
+            var studentList = {students: results}
+            res.render('class', studentList);
+        });
+    }else{
+        res.redirect("/account/login");
+    }
+});
+
+router.get('/newStudent' , (req, res, done) => {
+    if(req.isAuthenticated()){
+        db.classes.findAll({
+            include:[{
+                model: db.teachers,
+                attribute: [['name', 'teacher_userName']]
+            }],
+            where:{
+                teacherTeacherId: req.user.teacher_id,
+            },
+            order: db.sequelize.col('class_period')
+    }).then(function(results){
+        var classNames = {classes: results}
+        res.render('newStudent', classNames);
+        });
+    }else{
+        res.redirect("/account/login");
+    }        
+})
 
 module.exports = router;

7c4fe1387ca11ed9b9b735792b3a2b171d95b8bb
mjbelanic <mjbelanic@aol.com>
2017-10-11 17:37:39 -0700

    redirect to same page

--- controller/api.routes.js
+++ controller/api.routes.js
@@ -5,21 +5,21 @@ var db = require("../models");
 var passport = require("passport");
 
 // PUT route for updating student points via buttons
-router.put("/points/:id/:points", function(req, res) {
+router.put("/points/:class/:id/:points", function(req, res) {
         var points = parseInt(req.params.points);
         db.students.findById(req.params.id).then( student => {
             return student.increment( "student_score" , {by: points});
         }).then(function(results){
-            res.redirect("/dashboard");
+            res.redirect("/class/"+req.params.class);
         });
 });
 
-router.put("/hallpass/:id/:passes", function(req, res) {
+router.put("/hallpass/:class/:id/:passes", function(req, res) {
     var passes = parseInt(req.params.passes);
     db.students.findById(req.params.id).then( student => {
         return student.increment( "student_hallpass" , {by: passes});
     }).then(function(results){
-        res.redirect("/dashboard");
+        res.redirect("/class/"+req.params.class);
     });
 });
 
